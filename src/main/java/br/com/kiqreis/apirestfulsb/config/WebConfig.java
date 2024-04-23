be9068e5e0e90929f26b2480e3f65585a5d1f541
Kaique Reis <kreis1265@gmail.com>
2024-04-23 00:52:24 -0300

    yml file type bug fix and add cors

--- src/main/java/br/com/kiqreis/apirestfulsb/config/WebConfig.java
+++ src/main/java/br/com/kiqreis/apirestfulsb/config/WebConfig.java
@@ -1,6 +1,7 @@
 package br.com.kiqreis.apirestfulsb.config;
 
 import br.com.kiqreis.apirestfulsb.converter.YamlJackson2HttpMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
@@ -8,6 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,9 +19,24 @@ import java.util.List;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
+    private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
+
+    @Value("${cors.originsPatterns:default}")
+    private String corsOriginPatterns = "";
+
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(new YamlJackson2HttpMessageConverter());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        String[] allowedOrigins = corsOriginPatterns.split("\\s*,\\s*");
+
+        registry.addMapping("/**")
+                .allowedMethods("GET")
+                .allowedOrigins(allowedOrigins)
+                .allowCredentials(true);
     }
 
     @Override
@@ -30,7 +47,8 @@ public class WebConfig implements WebMvcConfigurer {
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
-                .mediaType("xml", MediaType.APPLICATION_XML);
+                .mediaType("xml", MediaType.APPLICATION_XML)
+                .mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML);
     }
 
     @Override
